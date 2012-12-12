ab8aea02f555f0d1faf92e31f7f02bb53e54cd90
unknown <mschuessler035@wit-xdvm-072.MILLARDPS.NET>
2012-12-12 11:25:13 -0600

    changes

---
+++ functions.php
@@ -0,0 +1,6 @@
+<?php
+// Register a single navigation menu, to be contructed 
+// in the admin panel, and displayed sin nav.php
+register_nav_menus(array(
+	'primary' =>'Primary Navigation'		
+));
--- index.php
+++ index.php
@@ -1,40 +1,42 @@
 <!DOCTYPE html>
 <html>
 	<head>
-		<link rel="stylesheet" type="text/css" href="<?php bloginfo('stylesheet_url') ?>" />
-	
-		<title><?php echo get_bloginfo('name') ?></title>
+		<?php get_header() ?>
 	</head>
 	<body>
-		<h1><?php bloginfo('name') ?></h1>
-		<P><?php bloginfo('description') ?></P>
-		
-		<div class="posts">
-		
-		<!-- Method #1
-			<?php if(have_posts()) :?>
-				<?php while(have_posts()) : ?>
-					<?php the_post() ?>
-					<h2><a href="<?php the_permalink() ?>"></a></h2>
-				<?php endwhile ?>
-			<?php endif ?> 
-						-->
-		
-		<!--  METHOD #2 
-		<?php 
-			if(have_posts) {
-				while(have_posts()) {
-					the_post(); // sets up *this* post's data
-					
-					// Display whateer information about the post tat you like
-					echo '<h2>';
-					the_title();
-					echo '</h2>';
-					the_content();
+		<div id="wrapper">
+			<header>
+				<h1><?php bloginfo('name') ?></h1>
+			</header>
+			<nav id="primary-nav">
+				<?php get_template_part('nav') // includes nav.php ?>
+			</nav>
+			<div id="main">
+				<?php 
+					/**
+					 * check to see if the current request...
+					 * is_category()
+					 * is_front_page()
+					 * is_home()
+					 * is_404()
+					 * is_archive()
+					 * is_page()
+					 * is_single() --> e.g. a single post
+					 * is_tag()
+					 * is_search() --> a page display search results
+					 */
+				if  (is_front_page()) {
+					// Include file name content-home.php
+					get_template_part('content','home');
+				} elseif (is_page() || is_single()) {
+					// Include file named content-single.php
+					get_template_part('content','single');
 				}
-			}
-		?>
-		-->
+				?>
+			</div>
+			<footer>
+				<?php get_footer() ?>
+			</footer>
 		</div>
 	</body>
 </html>
